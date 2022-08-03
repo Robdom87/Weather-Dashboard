ff9a93d961c22a7ac3c0b2057ea91733aa6bfae8
Cedar Chavez <geggas00@gmail.com>
2022-08-02 20:37:24 -0700

    add conceptual framework

---
+++ Develop/script.js
@@ -0,0 +1,13 @@
+//use day js to figure out day and shows appropriate weather info depending on that
+
+//add functionality to the search button and dynamically display all previous searches
+//save in local storage, limit of 8
+
+//once old search histories are clicked, then that cities info is displayed
+
+//color of UV index box change depending on conditions
+
+//display all weather conditions, update all info, and update icons accordinly
+
+//parse thru API outputs
+
